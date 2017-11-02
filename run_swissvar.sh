f474c6602133f7ed6e6fc54ef4ca7d3bf8ef6837
Hiren Joshi <hirenj@gmail.com>
2017-11-02 23:22:59 +0100

    Basic swissvar retrieval

--- run_swissvar.sh
+++ run_swissvar.sh
@@ -6,11 +6,12 @@ if [ ! -e dist ]; then
 	mkdir dist;
 fi
 
-curl 'http://swissvar.expasy.org/cgi-bin/swissvar/result?format=tab' > sources/swissvar.tsv
-if [ $? -gt 0 ]; then
-	exit 1
+if [ ! -e sources/swissvar.tsv ]; then
+	curl 'http://swissvar.expasy.org/cgi-bin/swissvar/result?format=tab' > sources/swissvar.tsv
+	if [ $? -gt 0 ]; then
+		exit 1
+	fi
 fi
-
 runrecipe --input sources --output json --env version="$TARGETVERSION" --env git="$GIT_STATUS" --env timestamp="$(date -u +%FT%TZ)" --env taxid="9606"
 mv json/sources_swissvar.json dist/swissvar-9606.json
 done
