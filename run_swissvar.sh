81368f85b8275ca1c1fcf33f366769a2f5aefbec
Hiren Joshi <hirenj@gmail.com>
2017-11-02 16:48:58 +0100

    Init

---
+++ run_swissvar.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+taxids=$1
+
+if [ ! -e dist ]; then
+	mkdir dist;
+fi
+
+curl 'http://swissvar.expasy.org/cgi-bin/swissvar/result?format=tab' > sources/swissvar.tsv
+if [ $? -gt 0 ]; then
+	exit 1
+fi
+
+runrecipe --input sources --output json --env version="$TARGETVERSION" --env git="$GIT_STATUS" --env timestamp="$(date -u +%FT%TZ)" --env taxid="9606"
+mv json/sources_swissvar.json dist/swissvar-9606.json
+done
